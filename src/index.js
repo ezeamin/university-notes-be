3c891e555fecc966b6937829f339ff9bd83b613a
Ezequiel Amin <ezequielamin@outlook.com>
2023-11-21 08:14:41 -0300

    update build script

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import { router } from './routes/routes.js';
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+//1- Inicializamos express
+const app = express();
+
+//2- Configuraciones del servidor -> Variables de servidor
+app.set('PORT', process.env.PORT || 5000);
+
+//3- Middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json()); // <==== parse request body as JSON
+
+//4- Rutas -> Tendra el siguiente formato
+app.use(router);
+
+//5- Loop del servidor
+app.listen(app.get('PORT'), () => {
+  console.log(`Servidor ejecutándose en puerto
+${app.get('PORT')}`);
+});
